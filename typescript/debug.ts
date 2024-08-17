32421628ac3fad07a767b3f050b01a40ba0e4f04
Mark Vasilkov <mvasilkov@gmail.com>
2024-08-17 15:40:51 +0300

    Palette

--- typescript/debug.ts
+++ typescript/debug.ts
@@ -2,16 +2,19 @@
 
 import { Settings } from './definitions.js'
 import { knightSVG } from './pieces.js'
-import { cellRefs } from './rendering.js'
+import { cellRefs, getColors } from './rendering.js'
 
 export const renderBoard = () => {
     for (let y = 0; y < Settings.boardHeight; ++y) {
         for (let x = 0; x < Settings.boardWidth; ++x) {
+            // Piece value (debug)
+            const value = (x + y * Settings.boardWidth) % 12 + 1
+
             const cell = cellRefs[y]![x]!
             const piece = document.createElement('div')
 
             piece.className = 'p'
-            piece.innerHTML = knightSVG('#94b0c2', '#1a1c2c', '#f4f4f4', '#566c86', '#333c57')
+            piece.innerHTML = knightSVG(...getColors(value))
 
             cell.appendChild(piece)
         }
--- typescript/rendering.ts
+++ typescript/rendering.ts
@@ -2,6 +2,41 @@
 
 import { Settings } from './definitions.js'
 
+const pieceColors = [
+    '#1a1c2c',
+    '#34223f',
+    '#4c2550',
+    '#63285d',
+    '#782d5d',
+    '#8c325c',
+    '#9e3859',
+    '#b03e54',
+    '#bd4854',
+    '#c95154',
+    '#d55c53',
+    '#df6755',
+    '#e87356',
+    '#ef7e57',
+    '#f48c5c',
+    '#f79a60',
+    '#faa765',
+    '#fcb36a',
+    '#fec070',
+    '#ffcd75',
+]
+
+type PieceColors = [color: string, outline: string, highlight: string, lowlight: string, lowlight2: string]
+
+export const getColors = (value: number): PieceColors => {
+    const color = pieceColors.at(-value - 3)!
+    const outline = pieceColors.at(0)!
+    const highlight = pieceColors.at(-value)!
+    const lowlight = pieceColors.at(-value - 6)!
+    const lowlight2 = pieceColors.at(-value - 8)!
+
+    return [color, outline, highlight, lowlight, lowlight2]
+}
+
 const getCellRefs = () => {
     const cellRefs: Element[][] = []
     const cells = document.querySelectorAll('.c')
