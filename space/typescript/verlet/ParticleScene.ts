7afea5aff2fce10a25c99d81433ea959a41091fb
Mark Vasilkov <mvasilkov@gmail.com>
2024-08-19 15:59:17 +0300

    ParticleScene

--- space/typescript/verlet/ParticleScene.ts
+++ space/typescript/verlet/ParticleScene.ts
@@ -1,5 +1,6 @@
 'use strict'
 
+import { register0, register1 } from '../../node_modules/natlib/Vec2.js'
 import type { Body } from '../../node_modules/natlib/verlet/Body'
 import type { Constraint } from '../../node_modules/natlib/verlet/Constraint'
 
@@ -23,4 +24,47 @@ export class ParticleScene {
         this.width = width
         this.iterationCount = iterationCount
     }
+
+    /** Update the scene. */
+    update() {
+        // Verlet integration
+        this.vertices.forEach(v => v.integrate())
+
+        // Solve constraints and collisions
+        for (let n = 0; n < this.iterationCount; ++n) {
+            this.constraints.forEach(c => c.solve())
+
+            for (let i = this.vertices.length; --i > 0;) {
+                const v0 = this.vertices[i]!
+
+                for (let j = i; --j >= 0;) {
+                    const v1 = this.vertices[j]!
+
+                    if (findCollision(v0, v1)) {
+                        resolveCollision(v0, v1)
+                    }
+                }
+            }
+        }
+    }
+}
+
+const findCollision = (v0: Particle, v1: Particle) =>
+    v0.position.distanceSquared(v1.position) < (v0.radius + v1.radius) ** 2
+
+const resolveCollision = (v0: Particle, v1: Particle) => {
+    const pos0 = v0.position
+    const pos1 = v1.position
+
+    const contactLine = register0 // .Alias
+    const contactDistance = contactLine.copy(pos1).subtract(pos0).length() - v0.radius - v1.radius
+
+    // Mass fractions
+    const totalMass = v0.gravity + v1.gravity
+    const w0 = v0.gravity / totalMass
+    const w1 = v1.gravity / totalMass
+
+    // Contact line points from v0 to v1
+    pos0.add(register1.setMultiplyScalar(contactLine.normalize(), w1 * contactDistance))
+    pos1.subtract(register1.setMultiplyScalar(contactLine, w0 * contactDistance))
 }
