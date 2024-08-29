edfb2862d81a4781525c1672ec3cf9813c04455a
Mark Vasilkov <mvasilkov@gmail.com>
2024-08-30 02:19:51 +0300

    Dakka

--- vr/app.js
+++ vr/app.js
@@ -1,1 +1,29 @@
 'use strict'
+
+AFRAME.registerComponent('dakka', {
+    init() {
+        this.el.addEventListener('click', event => {
+            const startPos = new THREE.Vector3()
+            this.el.object3D.getWorldPosition(startPos)
+
+            const { point: endPos, distance } = event.detail.intersection
+
+            const bullet = document.createElement('a-entity')
+            bullet.setAttribute('geometry', {
+                primitive: 'sphere',
+                radius: 0.05,
+            })
+            bullet.setAttribute('material', {
+                color: '#f00',
+            })
+            bullet.setAttribute('position', startPos)
+            bullet.setAttribute('animation', {
+                property: 'position',
+                to: endPos,
+                dur: 100 * distance,
+                easing: 'linear',
+            })
+            this.el.sceneEl.appendChild(bullet)
+        })
+    },
+})
