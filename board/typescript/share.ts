ccd477a42614f17a4e130d4a7770eea9a45b8c55
Mark Vasilkov <mvasilkov@gmail.com>
2024-09-09 17:32:01 +0300

    Fix share host

--- board/typescript/share.ts
+++ board/typescript/share.ts
@@ -1,10 +1,13 @@
 'use strict'
 
 export const shareTwitter = (succdef: 'SUCCESS' | 'DEFEAT', score: number) => {
+    const year = new Date().getFullYear()
+    const host = year > 2024 ? 'js13kgames.com' : 'dev.js13kgames.com'
+
     // https://developer.x.com/en/docs/x-for-websites/tweet-button/overview
     const intentUrl = 'https://twitter.com/intent/tweet'
     const text = `${succdef}! I scored ${score} in King Thirteen!`
-    const url = 'https://js13kgames.com/2024/games/king-thirteen'
+    const url = `https://${host}/2024/games/king-thirteen`
     const hashtags = 'KingThirteen,js13k'
     const via = 'mvasilkov'
 
